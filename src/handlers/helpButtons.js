5daa14be0c78ff34007ca73e143abcf30534f41e
nawaf4mu <nawaf4tu@gmail.com>
2026-09-21 19:43:35 +0300

    Update bug report button and embed content

--- src/handlers/helpButtons.js
+++ src/handlers/helpButtons.js
@@ -42,21 +42,15 @@ export const helpBugReportButton = {
     name: BUG_REPORT_BUTTON_ID,
     async execute(interaction, client) {
         const githubButton = new ButtonBuilder()
-            .setLabel('🐛 Report Bug on GitHub')
+            .setLabel('Na bot')
             .setStyle(ButtonStyle.Link)
-            .setURL('https://github.com/codebymitch/TitanBot/issues');
+            .setURL('https://discord.gg/XwRx6HFcNs');
 
         const bugRow = new ActionRowBuilder().addComponents(githubButton);
 
         const bugReportEmbed = createEmbed({
-            title: '🐛 Bug Report',
-            description: 'Found a bug? Please report it on our GitHub Issues page!\n\n' +
-                '**When reporting a bug, please include:**\n' +
-                '• 📝 Detailed description of the issue\n' +
-                '• 📋 Steps to reproduce the problem\n' +
-                '• 📸 Screenshots if applicable\n' +
-                '• 💻 Your bot version and environment\n\n' +
-                'This helps us fix issues faster and more effectively!',
+            title: '.',
+            description: 'nothing here',
             color: 'error'
         });
         bugReportEmbed.setFooter({
